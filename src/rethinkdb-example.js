a84463d9342961c29a439840180e923642291af2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-06 11:59:50 -0500

    increment and decrement

--- src/rethinkdb-example.js
+++ src/rethinkdb-example.js
@@ -7,6 +7,7 @@ function initStore() {
     .then(conn => {
       console.log('connected')
       const db = r.db('test')
+
       return db.tableList().run(conn)
         .then(tables => {
           if (tables.indexOf('state') !== -1) {
@@ -19,10 +20,12 @@ function initStore() {
             .then(() => console.log('created state table'))
         })
         .then(() => {
+          console.log('returning db objects')
           return {
+            r: r,
             conn: conn,
             db: db,
-            table: db.table('state')
+            table: r.db('test').table('state')
           }
         })
     })
@@ -35,20 +38,32 @@ function initStore() {
 function counter(rethinkState, action) {
   switch (action.type) {
   case 'INCREMENT':
-    return rethinkState.db
+    console.log('returning increment')
+    return rethinkState.table.get(1).update({
+      state: rethinkState.r.row('state').add(1)
+    }).run(rethinkState.conn).then(() => rethinkState)
   case 'DECREMENT':
-    return state - 1
+    console.log('returning decrement')
+    return rethinkState.table.get(1).update({
+      state: rethinkState.r.row('state').add(-1)
+    }).run(rethinkState.conn).then(() => rethinkState)
   default:
     // init on default
-    console.log('inserting default')
+    console.log('inserting default state')
     return rethinkState.table.insert({
       id: 1,
       state: 0
-    }).then(() => rethinkState)
+    }).run(rethinkState.conn).then(() => rethinkState)
   }
 }
 
 initStore()
+  .then(state => {
+    console.log('rethink state table insert', typeof state.table.insert)
+    return state
+  })
   .then(rethinkState => counter(rethinkState, {}))
   .then(rethinkState => counter(rethinkState, { type: 'INCREMENT' }))
+  .then(rethinkState => counter(rethinkState, { type: 'INCREMENT' }))
+  .then(rethinkState => counter(rethinkState, { type: 'DECREMENT' }))
   .done()
