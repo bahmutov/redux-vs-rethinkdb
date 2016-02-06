83e86610e3bfd02bf220f357fd730effcf5cf7b1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-06 11:00:28 -0500

    redux example

---
+++ dist/redux-example.compiled.for.5.js
@@ -0,0 +1,45 @@
+'use strict';
+require('pick-precompiled').babelPolyfill()
+
+var redux = require('redux');
+
+/**
+ * This is a reducer, a pure function with (state, action) => state signature.
+ * It describes how an action transforms the state into the next state.
+ *
+ * The shape of the state is up to you: it can be a primitive, an array, an object,
+ * or even an Immutable.js data structure. The only important part is that you should
+ * not mutate the state object, but return a new object if the state changes.
+ *
+ * In this example, we use a `switch` statement and strings, but you can use a helper that
+ * follows a different convention (such as function maps) if it makes sense for your project.
+ */
+function counter() {
+  let state = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+  let action = arguments[1];
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = redux.createStore(counter);
+
+// You can subscribe to the updates manually, or use bindings to your view layer.
+store.subscribe(() => console.log(store.getState()));
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'INCREMENT' });
+// 1
+store.dispatch({ type: 'INCREMENT' });
+// 2
+store.dispatch({ type: 'DECREMENT' });
+// 1
